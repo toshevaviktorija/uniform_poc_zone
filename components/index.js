07cb8c05aa1f277719b8305555369d2f11426304
viktorija <tosheva.victoria@yahoo.com>
2022-09-21 13:11:34 +0100

    remove featured product

--- components/index.js
+++ components/index.js
@@ -2,4 +2,3 @@ export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { Hero } from "./Hero";
 export { FeaturedEvent } from "./FeaturedEvent";
-export { FeaturedProduct } from "./FeaturedProduct";
